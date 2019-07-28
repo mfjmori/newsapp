1a10cc71d84ce67492f222add0e118ff35e0cebb
mfjmori <mfjmori@gmail.com>
2019-07-28 18:33:40 +0900

    add forceScheme

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+      if (\App::environment('production')) {
+        \URL::forceScheme('https');
+      }
       Schema::defaultStringLength(191);
       Blade::component('components.header', 'header');
       Blade::component('components.sub_header', 'sub_header');
