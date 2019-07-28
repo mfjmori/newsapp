6d7616272bb7fa5888d46d55a8f4be626d150f10
mfjmori <mfjmori@gmail.com>
2019-07-28 09:49:09 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,13 @@
 */
 
 Route::redirect('/', '/articles/technology', 301);
+Route::get('/home', 'HomeController@index')->name('home');
+
 Route::get('/articles/qiita', 'ArticleController@qiita')->name('articles.qiita');
 Route::get('/articles/{category?}', 'ArticleController@news')->name('articles.news');
-Route::resource('stocks', 'StockController', ['only' => ['index', 'store', 'destroy']]);
+Route::resource('stocks', 'StockController', ['only' => ['index', 'store', 'destroy']])->middleware('auth');
 
 Auth::routes();
 Route::namespace('Auth')->group(function () {
   Route::get('/logout', 'LoginController@getLogout');
 });
-
-Route::get('/home', 'HomeController@index')->name('home');
