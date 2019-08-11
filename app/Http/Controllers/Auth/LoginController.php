8148601355cfdaa4cd64bfcf711ebe6cccd0ac59
mfjmori <mfjmori@gmail.com>
2019-08-11 12:49:26 +0900

    change redirect path after login and authenticated user

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = '/articles/technology';
 
     /**
      * Create a new controller instance.
